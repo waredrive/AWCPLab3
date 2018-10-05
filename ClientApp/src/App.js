b21676e269bb00871fb50426a52f5ead3a01efb1
Lukas Sarnowski <lukas.sarnowski@gmail.com>
2018-10-05 10:51:27 +0200

    minor

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -5,10 +5,10 @@ export default class App extends Component {
 
   render() {
     return (
-      <>
+      <React.Fragment>
       <h1>THIS IS A HEADER!!!!</h1>
       <p>LOLZ!! ITS WORKING MAN!</p>
-      </>
+      </React.Fragment>
     );
   }
 }
