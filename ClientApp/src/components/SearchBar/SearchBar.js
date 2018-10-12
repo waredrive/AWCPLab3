22105918c90c96d9ad90835a1c040dcdb1353038
Lukas Sarnowski <lukas.sarnowski@gmail.com>
2018-10-12 15:42:44 +0200

    minor

--- ClientApp/src/components/SearchBar/SearchBar.js
+++ ClientApp/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { AsyncTypeahead } from 'react-bootstrap-typeahead';
+import { withRouter } from "react-router-dom";
 
 class SearchBar extends Component {
 	state = {
-		typeahead: {
+		typeaheadSettings: {
+			selectHintOnEnter: true,
 			isLoading: false,
 			options: [],
 			minLength: 3,
@@ -12,50 +14,52 @@ class SearchBar extends Component {
 		}
 	};
 
-	render() {
-		const typeahead = this.state.typeahead;
+	searchSelectedStation = station => {
+		if (station.length !== 1) {
+			return;
+		}
+		console.log('/',encodeURIComponent(station[0].Name.replace(/\//g, '_')), '/', station[0].SiteId)
+		this.props.history.push(`/${encodeURIComponent(station[0].Name.replace(/\//g, '_'))}/${station[0].SiteId}`);
+	};
 
+	fetchFromApi = query => {
+		let updatedTypeahead = { ...this.state.typeaheadSettings };
+		updatedTypeahead.isLoading = true;
+		this.setState({ typeaheadSettings: updatedTypeahead });
+		fetch('api/typeahead/' + query, {
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(resp => resp.json())
+			.then(response => {
+				let updatedTypeahead = { ...this.state.typeaheadSettings };
+				let filteredResponse = response.ResponseData.filter(val =>
+					val.Name.toLowerCase()
+						.trim()
+						.includes(query.toLowerCase().trim())
+				);
+				updatedTypeahead.isLoading = false;
+				updatedTypeahead.options = filteredResponse;
+				this.setState({
+					typeaheadSettings: updatedTypeahead
+				});
+			});
+	};
+
+	fetchFromSessionStorage = query => {};
+
+	render() {
 		return (
 			//TODO: Error Handling
 			<AsyncTypeahead
-				selectHintOnEnter={true}
-				isLoading={typeahead.isLoading}
-				onKeyPress={event => {
-					console.log(event);
-				}}
-				minLength={typeahead.minLength}
-				filterBy={typeahead.filterBy}
-				labelKey={typeahead.labelKey}
-				onChange={selected => console.log(selected)}
-				onSearch={query => {
-					let updatedTypeahead = { ...this.state.typeahead };
-					updatedTypeahead.isLoading = true;
-					this.setState({ typeahead: updatedTypeahead });
-					fetch('api/typeahead/' + query, {
-						headers: {
-							Accept: 'application/json',
-							'Content-Type': 'application/json'
-						}
-					})
-						.then(resp => resp.json())
-						.then(response => {
-							let updatedTypeahead = { ...this.state.typeahead };
-							let filteredResponse = response.ResponseData.filter(val =>
-								val.Name.toLowerCase()
-									.trim()
-									.includes(query.toLowerCase().trim())
-							);
-							updatedTypeahead.isLoading = false;
-							updatedTypeahead.options = filteredResponse;
-							this.setState({
-								typeahead: updatedTypeahead
-							});
-						});
-				}}
-				options={typeahead.options}
+				{...this.state.typeaheadSettings}
+				onChange={selected => this.searchSelectedStation(selected)}
+				onSearch={query => this.fetchFromApi(query)}
 			/>
 		);
 	}
 }
 
-export default SearchBar;
+export default withRouter(SearchBar);
