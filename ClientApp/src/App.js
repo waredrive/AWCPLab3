ac5fc32629511bba3f0834c5342fa5e57b7daaa1
Lukas Sarnowski <lukas.sarnowski@gmail.com>
2018-10-12 10:35:40 +0200

    before implementing typeahead

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import SearchBar from './components/SearchBar/SearchBar';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 
@@ -10,11 +11,11 @@ export default class App extends Component {
 		return (
 			<BrowserRouter basename={baseUrl}>
 				<SearchBar />
-				<Switch>
+				{/* <Switch>
 					<Route path="/:stationName/:stationId" component={SearchResults} />
 					<Route path="/error" component={() => {return <ErrorMessage message="An Error has occurred while fetching data from SL. Please try again."/>}} />
 					<Route component={WelcomePage} />
-				</Switch>
+				</Switch> */}
 			</BrowserRouter>
 		);
 	}
